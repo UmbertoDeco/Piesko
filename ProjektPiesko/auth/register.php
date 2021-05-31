13d63d0ceb1a5bdea63eec12f017e2ba752b00d8
Sławek Kuchta <chawek@gmail.com>
2021-05-31 21:57:22 +0200

    register form1

--- ProjektPiesko/auth/register.php
+++ ProjektPiesko/auth/register.php
@@ -12,14 +12,32 @@ require_once "../settings.php";
 <form method="post" action="">
    <div class="form-group">
       <label for="username">Username:</label>
-      <input id="username" class="form-control" type="text" maxlength="<?USER_USERNAME_MAX?>" name
-      </div>
+      <input id="username" class="form-control" type="text" maxlength="<?USER_USERNAME_MAX?>" name="username" required>
+   </div>
+   <div class="form-group">
+      <label for="email">Email:</label>
+      <input id="email" class="form-control" type="email" name="email" required>
+   </div>
+   <div class="form-group">
+      <label for="password">Password:</label>
+      <input id="password" class="form-control" type="password" name="password" required>
+   </div>
+   <div class="form-group">
+      <label for="confirm_password">Confirm password:</label>
+      <input id="confirm_password" class="form-control" type="password" name="confirm_password" required>
+   </div>
+   <div class="custom-control custom-checkbox">
+      <input id="accept_tos" class="custom-control-input" type="checkbox" name="true" required>
+      <label for="accept_tos" class="custom-control-label">I accept Terms of Service</label>
+   </div>
 
-      <div class="form group"> 
+   </form>
+
       
-      </div>
+      
    
-</form>
+   
+
 </div>
 <?php require_once BASE_DIR."/template/footer.php"?>
 </body>
